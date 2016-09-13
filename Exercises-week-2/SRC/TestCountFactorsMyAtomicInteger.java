4194e74ae4786a8f62cfebc98f11dc31e9434ef4
anb <anb@carecom.dk>
2016-09-13 16:02:06 +0200

    Week 2 Exercise 1

--- Exercises-week-2/SRC/TestCountFactorsMyAtomicInteger.java
+++ Exercises-week-2/SRC/TestCountFactorsMyAtomicInteger.java
@@ -1,20 +1,36 @@
-import java.util.concurrent.Thread;
-static import TestCountFactors.countFactors;
+import java.util.concurrent.atomic.AtomicInteger;
 public class TestCountFactorsMyAtomicInteger{
-	private final MyAtomicInteger counter = new MyAtomicInteger();
+	final static AtomicInteger counter = new AtomicInteger();
 	public static void main(String[] args){
+		long start = System.nanoTime();
+		parallelN(10, 5_000_000);
+		System.out.println("Time:" + (System.nanoTime() - start));
+		System.out.println(counter.get());
 
 	}
 
-	public static parallelN(int nThreads, int range){
-		int slice = range / nThreads;
-		Thread[] threads = new Thread[nThread];
-		for(int i = 0; i <= nThreads; i++){
-			Thread t = Thread(() -> {
-				for(while ){
+
+
+	public static void parallelN(int nThreads, int range){
+		final int slice = range / nThreads;
+		Thread[] threads = new Thread[nThreads];
+		for(int i = 0; i < nThreads; i++){
+			final int from = i*slice;
+			final int to= from +slice;
+			Thread t = new Thread( new Runnable() {
+				public void run(){
+					for(int p = from; p < to; p++){
+						counter.addAndGet(TestCountFactors.countFactors(p));			
+					}
 				}
 			});
+		threads[i] = t;
+		t.start();
 		}
+		try{
+			for(Thread t : threads)
+				t.join();
+		}catch(InterruptedException e){}
 	}
 
 
